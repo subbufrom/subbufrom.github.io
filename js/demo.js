411adfba130ec63adc7c665836a9cb969ed3b6fc
subbufrom <subbufrom@gmail.com>
2017-07-30 23:43:51 +0530

    Add files via upload

---
+++ js/demo.js
@@ -0,0 +1,27 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: 'hsl(210, 57%, 75%)',
+    lineColor: 'hsl(210, 57%, 75%)'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
---
+++ main.js
@@ -0,0 +1,6 @@
+console.log('Loaded!');
+
+var img = document.getElementById('madi');
+img.onclick = function() {
+    img.style.marginLeft = "100px";
+};
